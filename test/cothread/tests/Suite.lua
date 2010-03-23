ee4d9f6d60fa844b6ffa4cd5628072d82cbde277
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2010-03-23 00:35:58 +0000

    Some changes were mistakenly committed. Undoing them.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/loop/trunk@103362 ae0415b3-e90b-0410-900d-d0be9363c56b

--- test/cothread/tests/Suite.lua
+++ test/cothread/tests/Suite.lua
@@ -3,20 +3,20 @@ local utils = require "cothread.tests.utils"
 
 
 require "cothread"
-Labels = cothread.verbose.viewer.labels
+--Labels = cothread.verbose.viewer.labels
 --cothread.verbose:level(2)
-cothread.verbose:flag("socket", true)
+--cothread.verbose:flag("socket", true)
 --require "inspector"
 
 return Suite{
---	schedule = require "cothread.tests.schedule",
---	unschedule = require "cothread.tests.unschedule",
---	--notify = require "cothread.tests.notify",
---	--cancel = require "cothread.tests.cancel",
---	suspend = require "cothread.tests.suspend",
---	rescheduled = require "cothread.tests.rescheduled",
---	delay = require "cothread.tests.delay",
---	resume_blocked = require "cothread.tests.resume_blocked",
+	schedule = require "cothread.tests.schedule",
+	unschedule = require "cothread.tests.unschedule",
+	--notify = require "cothread.tests.notify",
+	--cancel = require "cothread.tests.cancel",
+	suspend = require "cothread.tests.suspend",
+	rescheduled = require "cothread.tests.rescheduled",
+	delay = require "cothread.tests.delay",
+	resume_blocked = require "cothread.tests.resume_blocked",
 	socket = require "cothread.tests.socket",
---	--select = require "cothread.tests.select"
+	--select = require "cothread.tests.select"
 }
