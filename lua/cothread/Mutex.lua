19d74754edce24601c069a2a684da2bdbabc346a
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2010-03-26 18:40:09 +0000

    Change to use CyclicSets to manage of threads waiting at the mutex (OrderedSet does not provide 'remove' anymore because it was slow (O(n))).
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/loop/trunk@103565 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/cothread/Mutex.lua
+++ lua/cothread/Mutex.lua
@@ -9,11 +9,10 @@ local yield = coroutine.yield
 local oo = require "loop.base"
 local class = oo.class
 
-local OrderedSet = require "loop.collection.OrderedSet"
-local contains = OrderedSet.contains
-local enqueue = OrderedSet.enqueue
-local head = OrderedSet.head
-local remove = OrderedSet.remove
+local BiCyclicSets = require "loop.collection.BiCyclicSets"
+local contains = BiCyclicSets.contains
+local add = BiCyclicSets.add
+local remove = BiCyclicSets.remove
 
 module(...)
 
@@ -26,7 +25,7 @@ function Mutex:try(wait)                                                        
 	if not inside then                                                            --[[VERBOSE]] verbose:mutex("resource is free")
 		self.inside = thread
 	elseif wait and inside ~= thread then                                         --[[VERBOSE]] verbose:mutex("resource in use: waiting for notification")
-		enqueue(self, thread)
+		add(self, thread, false)
 		yield("suspend")                                                            --[[VERBOSE]] verbose:mutex("notification received")
 		remove(self, thread)
 	end                                                                           --[[VERBOSE]] verbose:mutex(false, "access ",(self.inside == thread) and "granted" or "denied")
@@ -35,7 +34,7 @@ end
 
 function Mutex:free()                                                           --[[VERBOSE]] local verbose = yield("verbose")
 	if self.inside == running() then
-		local thread = head(self)
+		local thread = self[false]
 		if thread then
 			self.inside = thread                                                      --[[VERBOSE]] verbose:mutex("resouce released for ",thread)
 			yield("resume", thread)
@@ -46,19 +45,19 @@ function Mutex:free()                                                           
 	end                                                                           --[[VERBOSE]] verbose:mutex("attempt to release resource not owned")
 end
 
-function Mutex:deny(thread)                                                     --[[VERBOSE]] local verbose = yield("verbose")
-	if contains(self, thread) then                                                --[[VERBOSE]] verbose:mutex("deny access for ",thread)
+function Mutex:deny(thread, ...)                                                --[[VERBOSE]] local verbose = yield("verbose")
+	if self[thread] ~= nil then                                                   --[[VERBOSE]] verbose:mutex("deny access for ",thread)
 		yield("resume", thread)
 		return true
 	end                                                                           --[[VERBOSE]] verbose:mutex("attempt to deny access for a thread not interested")
 end
 
-function Mutex:grant(thread)                                                    --[[VERBOSE]] local verbose = yield("verbose")
+function Mutex:grant(thread, ...)                                               --[[VERBOSE]] local verbose = yield("verbose")
 	if self.inside == running()
-	and contains(self, thread)
+	and self[thread] ~= nil
 	then
 		self.inside = thread                                                        --[[VERBOSE]] verbose:mutex("access resource granted for ",thread)
-		yield("resume", thread)
+		yield("resume", thread, ...)
 		return true                                                                 --[[VERBOSE]] else verbose:mutex("attempt to grant resource access for ",thread," failed")
 	end
 end
