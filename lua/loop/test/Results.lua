4f7a2f73af247240e4350085f6bd820e0780cfac
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2010-03-31 17:57:28 +0000

    Uses 'xpcall' and 'debug.traceback' from 'cothread.auxiliary' that allow to pass values to the function being called.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/loop/trunk@103785 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/loop/test/Results.lua
+++ lua/loop/test/Results.lua
@@ -1,18 +1,22 @@
 
-local luapcall = pcall
-local select   = select
+local _G = require "_G"
+local select = _G.select
+local xpcall = _G.xpcall
 
 local table = require "table"
+local concat = table.concat
+
+local debug = require "debug"
+local traceback = debug.traceback
 
 local oo = require "loop.simple"
+local class = oo.class
 
 local Assert = require "loop.test.Assert"
 
 module "loop.test.Results"
 
-oo.class(_M, Assert)
-
-pcall = luapcall
+class(_M, Assert)
 
 function process(self, label, name, success, ...)
 	if self.reporter then
@@ -24,9 +28,9 @@ end
 
 function test(self, label, func, ...)
 	self[#self+1] = label
-	local name = table.concat(self, ".")
+	local name = concat(self, ".")
 	if self.reporter then
 		self.reporter:started(name)
 	end
-	return self:process(label, name, self.pcall(func, ...))
+	return self:process(label, name, xpcall(func, traceback, ...))
 end
