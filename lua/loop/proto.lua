e62e26962f3860bd06883b407b3ca19e0ed303fc
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2011-04-13 18:21:25 +0000

    Adjustment to use the new policy of Lua 5.2 modules.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/loop/trunk@117233 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/loop/proto.lua
+++ lua/loop/proto.lua
@@ -3,29 +3,30 @@
 -- Title  : Dymamic Prototyping Model
 -- Author : Renato Maia <maia@inf.puc-rio.br>
 
+local _G = require "_G"
+local getmetatable = _G.getmetatable
+local rawget = _G.rawget
+local setmetatable = _G.setmetatable
+
 local table = require "loop.table"
 local memoize = table.memoize
 
-local base = require "loop.base"
-local class = base.class
-local getclass = base.getclass
-local isinstanceof = base.isinstanceof
+local CloneOf = memoize(function(proto) return {__index = proto} end, "k")
 
-module "loop.proto"
-
-local CloneOf = memoize(function(proto) return class{__index = proto} end, "k")
-
-function clone(proto, clone)
-	return CloneOf[proto](clone)
-end
-
-function getproto(clone)
-	local class = getclass(clone)
-	if class ~= nil then
-		return class.__index
-	end
-end
-
-function iscloneof(clone, proto)
-	return isinstanceof(clone, CloneOf[proto])
-end
+return {
+	clone = function(proto, clone)
+		return setmetatable(clone, CloneOf[proto])
+	end,
+	getproto = function(clone)
+		local meta = getmetatable(clone)
+		if meta ~= nil then
+			local proto = meta.__index
+			if meta == rawget(CloneOf, proto) then
+				return proto
+			end
+		end
+	end,
+	iscloneof = function(clone, proto)
+		return getmetatable(clone) == rawget(CloneOf, proto)
+	end,
+}
