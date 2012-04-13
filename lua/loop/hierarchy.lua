f12d050b73184e075f48a14f39a99ee3e5d0a5bb
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2012-04-13 21:00:48 +0000

    - Bugfix in the implementation of the class hierarchy topdown iterator. - New test for the class hierarchy topdown iterator.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/loop/trunk@128119 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/loop/hierarchy.lua
+++ lua/loop/hierarchy.lua
@@ -2,33 +2,29 @@
 -- Title  : 
 -- Author : Renato Maia <maia@inf.puc-rio.br>
 
+local coroutine = require "coroutine"
+local wrap = coroutine.wrap
+local yield = coroutine.yield
+
 local oo = require "loop"
 local getmember = oo.getmember
 local rawnew = oo.rawnew
 local supers = oo.supers
 
-local CyclicSets = require "loop.collection.CyclicSets"
-
 module(...)
 
-local function depthfirst(stack, prev)
-	local class = stack[prev]
-	while class do
-		local superclass
-		for _, super in supers(class) do
-			if stack:add(super, prev) == super then
-				superclass = super
-			end
-		end
-		if not superclass then return class end
-		class = superclass
+local function yieldsupers(history, class)
+	for _, super in supers(class) do
+		yieldsupers(history, super)
+	end
+	if history[class] == nil then
+		history[class] = true
+		yield(class)
 	end
 end
 
 function topdown(class)
-	local stack = CyclicSets()
-	stack:add(class, false)
-	return depthfirst, stack, false
+	return wrap(yieldsupers), {}, class
 end
 
 function creator(class, ...)
