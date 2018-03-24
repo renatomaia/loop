9caeffbfe8d3f0f18c9c3ba64d17c79937e6c700
Renato Maia <maia.renato@gmail.com>
2018-03-24 11:29:31 -0300

    Publishing original LOOP website as GitHub Pages.

--- test/testall.lua
+++
@@ -1,8 +0,0 @@
-lua loop/tests/models/proto.lua
-lua loop/tests/models/base.lua
-lua loop/tests/models/simple.lua
-lua loop/tests/models/multiple.lua
-lua loop/tests/models/cached.lua
-#lua loop/tests/models/static.lua
-lua loop/tests/models/hierarchy.lua
-
